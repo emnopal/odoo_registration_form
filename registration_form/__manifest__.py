7cd3993980bb8ad62e7820cc5e7132e2dd975acd
Muhammad Naufal <mnaufal0999@gmail.com>
2022-02-09 15:53:15 +0700

    fixing odoo modules

--- registration_form/__manifest__.py
+++ registration_form/__manifest__.py
@@ -1,21 +1,51 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Registration Form',
+
+    # Version of module, if you set to 1.0 then it will be {odoo_version}.1.0. ex: 14.0.1.0
+    # If you set to 14.0.1.0.5 then it still 14.0.1.0.5
     'version': '1.0',
+
     'summary': 'Simple Registration Form',
+
+    # sequence, where to put the module in the menu
     'sequence': -100,
+
     'description': """Simple Registration Form""",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
     'category': 'Management',
+
     'website': '',
+
     'license': '',
+
+    # any module necessary for this one to work correctly
     'depends': [],
+    # By default, all modules will inherit from base module of odoo
+
+    # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        # this need following code convention,
+        # first is the security rule
+        # if group_id:id is empty, then access will be grant globally
+        # 1: True, 0: False
+        'security/ir.model.access.csv',  # this is required for the registration form to work
         'views/regis.xml'
     ],
+
+    # only loaded in demonstration mode
     'demo': [],
+
     'qweb': [],
+
+    # If it's True, the modules will available in the addons menu to install
     'installable': True,
+
     'application': True,
+
+    # If it's True, the modules will be auto-installed when all dependencies are installed
     'auto_install': False,
 }
--- registration_form/models/regis.py
+++ registration_form/models/regis.py
@@ -1,18 +1,50 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
+
+# To upgrade models you need to restart the server
+from odoo import api, fields, models, _
 
 
 class FormRegist(models.Model):
     _name = "form.regist"
     _description = "Form Regist"
+    # if you want to inherit
+    # _inherit = "{Model you want to inherit}"
+    _rec_name = 'fullname'
 
-    first_name = fields.Char(string='First Name', required=True)
-    last_name = fields.Char(string='Last Name', required=True)
-    age = fields.Integer(string='Age')
+    # specify the fields you want to add on inherit model
+    first_name = fields.Char(string='First Name', required=True, size=64)
+    last_name = fields.Char(string='Last Name', required=True, size=64)
+    age = fields.Integer(string='Age', size=5)
     gender = fields.Selection([
         ('male', 'Male'),
-        ('female', 'Female'),
-        ('other', 'other'),
+        ('female', 'Female')
     ], required=True, default='male')
     address = fields.Text(string='Address')
+    bio = fields.Text(string='Bio')
+    fullname = fields.Char(string='Fullname', compute='_compute_fields_combination')
 
+    @api.depends('first_name', 'last_name')
+    def _compute_fields_combination(self):
+        for com in self:
+            if com.first_name:
+                com.fullname = f"{str(com.first_name).title()}"
+                if com.last_name:
+                    com.fullname = f"{str(com.first_name).title()} {str(com.last_name).title()}"
+            else:
+                com.fullname = ""
+
+    @api.onchange('first_name')
+    def set_first_name_caps(self):
+        if self.first_name:
+            val = str(self.first_name)
+            self.first_name = val.title()
+        else:
+            self.first_name = ""
+
+    @api.onchange('last_name')
+    def set_last_name_caps(self):
+        if self.last_name:
+            val = str(self.last_name)
+            self.last_name = val.title()
+        else:
+            self.last_name = ""
